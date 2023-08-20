ae4a99d8a16f278eea13d98378800a6605670090
joseedivandro <jose.edivandro.ja@outlook.com>
2023-08-20 10:43:01 -0300

    feat(tweets): criação dos tweets e users arquitetado

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { createTweetDto, createPageDto } from './dtos/tweet.dto';
+import { CreateUserDto } from './dtos/user.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +19,35 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/health')
+  getHealth(): string {
+    return this.appService.getHealth();
+  }
+
+  @Post('/sign-up')
+  @HttpCode(HttpStatus.OK)
+  postUsers(@Body() body: CreateUserDto): void {
+    this.appService.postUsers(body);
+  }
+
+  @Post('/tweets')
+  postTweets(@Body() body: createTweetDto): void {
+    this.appService.postTweets(body);
+  }
+
+  @Get('/tweets')
+  getTweets(@Query() page: createPageDto) {
+    const pageNumber = Number(page.page);
+
+    return this.appService.getTweets(pageNumber);
+  }
+
+  @Get('/tweets/:username')
+  getUserTweets(@Param() user: { username: string }) {
+    const username = user.username;
+
+    return this.appService.getUserTweets(username);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,79 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, HttpStatus, HttpException } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { Tweet } from './entities/tweet.entity';
 @Injectable()
 export class AppService {
+  private users: User[]; //persistencia de dados em memoria
+  tweets: Tweet[];
+
+  constructor() {
+    this.users = [];
+    this.tweets = [];
+  }
+
   getHello(): string {
-    return 'Hello World!';
+    return "I'm okay!";
+  }
+
+  getHealth(): string {
+    return "I'm okay!";
+  }
+
+  postUsers(user: User): void {
+    this.users.push(user);
+  }
+
+  postTweets(tweet: Tweet): void {
+    let checkSignUp: boolean = false;
+
+    this.users.forEach((user) => {
+      if (tweet.username === user.username) checkSignUp = true;
+    });
+
+    if (!checkSignUp)
+      throw new HttpException('UNAUTHORIZED', HttpStatus.UNAUTHORIZED);
+
+    this.tweets.push(tweet);
+  }
+
+  getTweets(page: number) {
+    const tweets = [];
+    const avatarHash = {};
+
+    this.users.forEach((user) => {
+      avatarHash[user.username] = user.avatar;
+    });
+
+    for (
+      let i = 15 * (isNaN(page) ? 0 : page - 1);
+      i < 15 * (isNaN(page) ? 1 : page);
+      i++
+    ) {
+      if (this.tweets[i])
+        tweets.push({
+          ...this.tweets[i],
+          avatar: avatarHash[this.tweets[i].username],
+        });
+    }
+
+    return tweets;
+  }
+
+  getUserTweets(username: string) {
+    const tweets = [];
+    let avatar: string;
+
+    this.users.forEach((user) => {
+      if (user.username === username) avatar = user.avatar;
+    });
+
+    this.tweets.forEach((tweet) => {
+      if (tweet.username === username)
+        tweets.push({
+          ...tweet,
+          avatar,
+        });
+    });
+    return tweets;
   }
 }
